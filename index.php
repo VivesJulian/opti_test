87715a39d266629af3eb9f2474282c6632e2eae4
VivesJulian <julian.vives66@gmail.com>
2024-08-04 20:05:21 +0200

    e

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+
+    <!-- chargement de Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
+    <!-- chargement de Brython -->
+    <script type="text/javascript"
+        src="https://cdn.jsdelivr.net/npm/brython@3.12.4/brython.min.js">
+    </script>
+    <script type="text/javascript"
+        src="https://cdn.jsdelivr.net/npm/brython@3.12.4/brython_stdlib.js">
+    </script>
+</head>
+<body>
+
+
+    <div class="container">
+        <div class="col-md-3">
+            <button type="button" class="btn btn-primary">Primary</button>
+        </div>
+        
+        
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">date</th>
+                <th scope="col">EUR</th>
+                <th scope="col">USD</th>
+                <th scope="col">JPY</th>
+                <th scope="col">GBP</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                </tr>
+                <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                </tr>
+                <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
