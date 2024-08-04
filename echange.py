37f3a79a4bb4a2f795300106f9113dc5bc8001e5
VivesJulian <julian.vives66@gmail.com>
2024-08-04 20:08:32 +0200

    Create echange.py

---
+++ echange.py
@@ -0,0 +1,8 @@
+import requests
+
+def req_Api(code, )
+
+    url = "https://openexchangerates.org/api/historical/2022-07-25.json?app_id="+code
+
+    reponse = requests.get(url)
+    print(reponse)
